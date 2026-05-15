ed3b9774f7ad7719224c0c8794e91e083decb869
Heatrayy <115377416+Heatrayy@users.noreply.github.com>
2026-05-15 11:15:07 -0600

    Create Lab03-Carlos Diaz-1348426-Ejercicio 1.cs

---
+++ Lab03/Lab03-Carlos Diaz-1348426-Ejercicio 1.cs
@@ -0,0 +1,12 @@
+Console.Write("Ingrese Nombre del Alumno: ");
+string nombreAlumno = Console.ReadLine() ?? "";
+
+Console.Write("Ingresar nombre del curso: ");
+string nombreCurso = Console.ReadLine() ?? "";
+
+Console.WriteLine();
+Console.WriteLine($"Alumno: {nombreAlumno}");
+Console.WriteLine($"Curso: {nombreCurso}");
+Console.WriteLine($"Carlos, Vas bien en tu curso de pensamiento computacional");
+Console.WriteLine($"Nunca te rindas sin importar el como te sientas");
+
